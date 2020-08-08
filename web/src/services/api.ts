b6f2837c1dd75a6dbd142a59fbf14b78425af122
Wilson Santos <contato@wilsonsantos.net>
2020-08-08 17:03:08 -0300

    Change BaseURL axios for external connection ngrok

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,15 @@
 import axios from 'axios';
 
+//For NGROK remeber change url on .ENV file.
+const URL = window.location.hostname.includes('localhost')
+? 'http://localhost:3333'
+: process.env.REACT_APP_DOMAIN;
+
 const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    
+    baseURL: URL,
 })
+
+
 
 export default api;
